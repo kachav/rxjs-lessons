78e0ffb47ce6da249560af9a8c862a3d69df1499
kachesov-a <kachesov-a@yandex-team.ru>
2020-02-16 22:22:39 +0300

    retry-retry-when

--- src/epics/dataFetch/index.js
+++ src/epics/dataFetch/index.js
@@ -1,14 +1,24 @@
 import {ofType} from 'redux-observable';
-import {switchMap, catchError, map} from 'rxjs/operators';
+import {switchMap, catchError, map, retry, retryWhen, delay, take} from 'rxjs/operators';
 import {from, of} from 'rxjs';
 
 import {FETCH_DATA, fetchDataFulfilled, fetchDataFailed} from '../../actions/dataFetch';
 
-const fetchData = () => fetch('./response.json').then(res => res.json());
+const fetchData = () => fetch('./response2.json').then(res => res.json());
+
+const makeApi = (apiMethod) => (...args) => of(args).pipe(
+    switchMap(args => from(apiMethod(args))),
+);
+
+const fetchApi = makeApi(fetchData);
 
 export default action$ => action$.pipe(
     ofType(FETCH_DATA),
-    switchMap(() => from(fetchData()).pipe(
+    switchMap(() => fetchApi().pipe(
+        retryWhen(errors$ => errors$.pipe(
+            delay(500),
+            take(5),
+        )),
         map(fetchDataFulfilled),
         catchError(error => of(fetchDataFailed(error))),
     ))
